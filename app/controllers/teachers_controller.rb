e0486e62ad05045ed9c7baf2ef10a0f4fce839b6
MohamedTab <m.tabib@live.fr>
2014-11-24 17:43:46 +0100

    ajout user can sign in as a teacher and create a teacher profil

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,11 +1,18 @@
 class TeachersController < ApplicationController
+    before_action :set_school, only: [:show, :edit, :update, :destroy,]
   def index
+    @teachers = Teacher.all
   end
 
   def new
+    @teacher = Teacher.new
   end
 
   def create
+    @school = School.find(params[:school_id])
+    @teacher = @school.teacher.new(params[:teacher])
+    @teacher.save
+    redirect_to teachers_path
   end
 
   def show
@@ -18,5 +25,15 @@ class TeachersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_school
+    @teacher = Teacher.find(params[:id])
+  end
+
+  def teacher_params
+    params.require(:teacher).permit(:name)
   end
 end
