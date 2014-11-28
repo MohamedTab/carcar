cbb3fb1facd3930da0187425aa547a4903ec4a4d
MohamedTab <m.tabib@live.fr>
2014-11-28 11:57:53 +0100

    ajout de lesson possible via le teacher

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -12,7 +12,6 @@ class TeachersController < ApplicationController
   end
 
   def show
-    @hours = ["06:00 - 07:00","07:00 - 08:00","08:00 - 09:00","09:00 - 10:00","10:00 - 11:00","11:00 - 12:00", "12:00 - 13:00", "13:00 - 14:00", "14:00 - 15:00", "15:00 - 16:00", "16:00 - 17:00", "17:00 - 18:00", "18:00 - 19:00", "19:00 - 20:00", "20:00 - 21:00", "21:00 - 22:00", "22:00 - 23:00"]
   end
 
   def update
@@ -31,3 +30,5 @@ class TeachersController < ApplicationController
     params.require(:teacher).permit(:name)
   end
 end
+
+
