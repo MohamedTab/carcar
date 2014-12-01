e7a938f368749eb5247a740c974dde158c6f7eb0
MohamedTab <m.tabib@live.fr>
2014-12-01 10:31:26 +0100

    booking ok

--- app/controllers/teachers/registrations_controller.rb
+++ app/controllers/teachers/registrations_controller.rb
@@ -7,4 +7,12 @@ class Teachers::RegistrationsController < Devise::RegistrationsController
    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name, :zip, :address, :city, :picture) }
    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :name, :zip, :address, :city, :picture) }
   end
+
+  def after_sign_up_path_for(teacher)
+    teacher_path(teacher)
+  end
+
+  def after_sign_in_path_for(teacher)
+    teacher_path(teacher)
+  end
 end
