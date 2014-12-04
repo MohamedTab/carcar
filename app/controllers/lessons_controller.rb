f245857ef6d436206232aebaa9c5f098b1aacaec
martinchenu <martinchenu74@hotmail.com>
2014-12-04 15:45:03 +0100

    lesson controller fix

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,20 +1,17 @@
 class LessonsController < InheritedResources::Base
 
   def create
-    if learner_signed_in?
       lesson = current_learner.lessons.build(lesson_params)
       teacher_id = params[:teacher_id]
       if lesson.find_availability_for_teacher(teacher_id)
         lesson.save
         redirect_to learner_path(current_learner)
       else
-      # AJOUTER MESSAGE DERREUR
         flash[:error] = "Sa marsh pa lol NEGER"
         redirect_to learner_path(current_learner)
       end
-    elsif school ...
-    end
   end
+
 
   private
 
