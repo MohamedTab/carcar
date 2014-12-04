ca4fb48b1f7b50124a6c1786fcb82bdfbd025c41
martinchenu <martinchenu74@hotmail.com>
2014-12-04 14:58:42 +0100

    eilleur show des teachers

--- app/controllers/learners/registrations_controller.rb
+++ app/controllers/learners/registrations_controller.rb
@@ -5,7 +5,7 @@ class Learners::RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :firs_name, :last_name, :date_of_birth, :phone, :address, :city, :zip, :country, :password, :picture) }
-  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :firs_name, :last_name, :date_of_birth, :phone, :address, :city, :zip, :country, :password, :picture) }
+  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :firs_name, :last_name, :date_of_birth, :phone, :address, :city, :zip, :country, :password, :picture, :current_password) }
   end
 
   def after_sign_up_path_for(learner)
