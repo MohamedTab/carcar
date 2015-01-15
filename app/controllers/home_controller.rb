90088970e2d40ef42d78242437cdb758bb537258
MohamedTab <m.tabib@live.fr>
2015-01-15 16:50:23 +0100

    added search homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    if params[:locality]
+      @schools = School.search(params[:locality])
+    else
+    @schools = School.order("created_at DESC")
+    end
     @schools = School.all
     @markers = Gmaps4rails.build_markers(@schools) do |school, marker|
       marker.lat school.latitude
